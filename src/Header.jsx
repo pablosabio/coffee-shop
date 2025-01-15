14ef066f4902b5d0726064e2ac9fd8d8df94634e
pablo <pablo.sabio@dci-student.org>
2025-01-15 16:19:01 +0100

    Header v1

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,49 @@
+import "./Header.css";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faHeart, faUser, faBagShopping } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const [activeLink, setActiveLink] = useState("Home");
+
+  const handleActiveLink = (link) => {
+    setActiveLink(link);
+  };
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  )
+    <header>
+      <nav>
+        <ul className="nav-links">
+          {["Home", "Shop", "About Us", "Contact Us"].map((link) => (
+            <li
+              key={link}
+              className={activeLink === link ? "active" : ""}
+              onClick={() => handleActiveLink(link)}
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+
+        <div className="logo">
+          <img src="/images/logo.png" alt="Logo" />
+        </div>
+
+        <div className="icons">
+          <span>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faHeart} />
+          </span>
+          <span>
+          <FontAwesomeIcon icon={faUser} />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faBagShopping} />
+          </span>
+        </div>
+      </nav>
+    </header>
+  );
 }
