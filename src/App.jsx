1fb7f7f709e215d774cfd3f8ce9bfc025105a5ff
Abdulrahman <abdulrahman.almhawsh@dci-student.or>
2025-01-15 11:11:18 +0100

    Creating the section V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import Header from "./Header"
+/* eslint-disable react/prop-types */
+import "./App.css";
 
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-      <Header/>
-    </>
-  )
-}
+    <div className="app-container">
+      <header className="header">
+        <h1>Exploring Our Unique Collections</h1>
+      </header>
+      <div className="Cards">
+      <Card image="https://150698241.v2.pressablecdn.com/coffee-store/wp-content/uploads/sites/341/2024/11/ct1-1.png" title="Beautiful Sunset" />
+      <Card image="https://150698241.v2.pressablecdn.com/coffee-store/wp-content/uploads/sites/341/2024/11/ct1-1.png" title="Beautiful Sunset" />
+      <Card image="https://150698241.v2.pressablecdn.com/coffee-store/wp-content/uploads/sites/341/2024/11/ct1-1.png" title="Beautiful Sunset" />
+      </div>
 
-export default App
+    </div>
+  );
+};
+
+export default App;
+
+function Card({image,title}) {
+    return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={image} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+      </div>
+    </div>
+  );
+}
