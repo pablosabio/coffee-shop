704ecdf824ec5ccf91be6fa60c06bcd88b932e6d
Royce <royce.burghardt@dci-student.org>
2025-01-18 23:15:57 +0100

    trial footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Footer from './components/Footer';
-import Footer2 from './components/Footer2';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div>
-    
-      <Footer2/>
+      <Footer />
+      <Routes>
+      {/*}  <Route path="/tracking" element={<Tracking />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        {/* Add other routes */}
+      </Routes>
     </div>
+  </Router>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { FaFacebook, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa';
 
 function Footer  () {
@@ -46,6 +46,70 @@ function Footer  () {
   );
 };
 
+export default Footer;*/
+
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "#D2B48C", padding: "20px 40px", color:"white", }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        {/* About Us Section */}
+        <div style={{ flex: "1" }}>
+          <h3>About Us</h3>
+          <p>
+            Suscipit ornare tortor turpis mus rhoncus iaculis montes hendrerit,
+            phasellus inceptos non ante sociis facilisi blandit. Sapien mattis
+            eleifend ultrices venenatis donec, purus malesuada sociosqu est.
+          </p>
+        </div>
+
+        {/* Quick Links Section */}
+        <div style={{ flex: "1" }}>
+          <h3>Quick Links</h3>
+          <ul style={{ listStyleType: "none", padding: "0" }}>
+            <li><Link to="/Versand" style={{color: "white"}}>Versand</Link></li>
+            <li><Link to="/privacy-policy" style={{color: "white"}}>Privacy Policy</Link></li>
+            <li><Link to="/terms-conditions" style={{color: "white"}}>Terms & Conditions</Link></li>
+            <li><Link to="/return" style={{color: "white"}}>Return</Link></li>
+          </ul>
+        </div>
+
+        {/* Contact Details Section */}
+        <div style={{ flex: "1" }}>
+          <h3>Contact Details</h3>
+          <address>
+            49 Featherstone Street<br />
+            London EC1Y8SY United Kingdom<br />
+            Toll Free No. 1800-384-4854<br />
+            Email Id: <a href="mailto:Coffbrew@Yahoo.com">Coffbrew@Yahoo.com</a>
+          </address>
+        </div>
+      </div>
+
+      {/* Social Media Icons */}
+      <div style={{ marginTop: "20px", display: "flex", justifyContent: "center", gap: "20px" }}>
+        <Link to="/">
+          <FaFacebook size={30} />
+        </Link>
+        <Link to="/">
+          <FaInstagram size={30} />
+        </Link>
+        <Link to="/">
+          <FaLinkedin size={30} />
+        </Link>
+        <Link to="/">
+          <FaYoutube size={30} />
+        </Link>
+      </div>
+    </footer>
+  );
+}
+
 export default Footer;
+
+
 
 
