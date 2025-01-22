be8a61357c8740dac91e2baecc844a4df6a8b9fe
Abdulrahman <abdulrahman.almhawsh@dci-student.or>
2025-01-22 15:35:10 +0100

    all

--- src/Sections/Next_Exploring_Our_Unique.jsx
+++ src/Sections/Next_Exploring_Our_Unique.jsx
@@ -3,26 +3,40 @@ import Card from "../components/Card"
 function Next_Exploring_Our_Unique () {
   
     return (
-      <div className="app-container bg-[#f3daa2] text-[##6d4c41] font-sans text-center ">
-        <div className="relative bg-cover bg-center bg-[url('https://www.nescafe.com/gb/sites/default/files/2023-11/Untitled-5%20copy_6.jpg')]">
-        <div className="absolute inset-0 bg-[#FFF1D3]/80"></div> 
-  
-  
-          <div className="min-h-screen flex items-center justify-center">
-  
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 min-h-screen text-center items-center justify-center relative ">
-              <Card image=".\images\hazelnut-harmony.png" title="Hazelnut Harmony" button_name="Add to cart" price="119$"/>
-              <Card image=".\images\bistro-reserve.png" title="Sunrise Blend" button_name="Add to cart" price="69$"/>
-              <Card image=".\images\sunrise-blend.png" title="Morning Symphony" button_name="Add to cart" price="55$"/>
-              <Card image=".\images\vanila-cloudburst.png" title="Vanilla Cloudburst" button_name="Add to cart" price="79$"/>
-              <Card image=".\images\harvest-blend.png" title="Harvest Blend" button_name="Add to cart" price="90$"/>
-              <Card image=".\images\velvet-fusion.png" title="Velvet Fusion" button_name="Add to cart" price="98$"/>
-              <Card image=".\images\peak-blend.png" title="Peak Blend" button_name="Add to cart" price="80$"/>
-              <Card image=".\images\morning-symphony.png" title="Bistro Reserve" button_name="Add to cart" price="79$"/>
-            </div>
-          </div>
-        </div>
+      <div className="app-container bg-[#f3daa2] text-[#6d4c41] font-sans text-center">
+  {/* الخلفية بالفيديو */}
+  <div className="relative bg-cover bg-center">
+    {/* عنصر الفيديو */}
+    <video 
+      autoPlay 
+      loop 
+      muted 
+      playsInline 
+      className="absolute inset-0 w-full h-full object-cover z-0">
+      <source src="https://videos.pexels.com/video-files/19005044/19005044-hd_1920_1080_24fps.mp4" type="video/mp4" />
+      {/* رسالة في حالة عدم دعم الفيديو */}
+      Your browser does not support the video tag.
+    </video>
+
+    {/* الطبقة النصف شفافة */}
+    <div className="absolute inset-0 bg-[#FFF1D3]/80 z-5"></div>
+
+    {/* المحتوى */}
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 min-h-screen text-center items-center justify-center relative z-10">
+        <Card image=".\images\hazelnut-harmony.png" title="Hazelnut Harmony" button_name="Add to cart" price="119$" />
+        <Card image=".\images\bistro-reserve.png" title="Sunrise Blend" button_name="Add to cart" price="69$" />
+        <Card image=".\images\sunrise-blend.png" title="Morning Symphony" button_name="Add to cart" price="55$" />
+        <Card image=".\images\vanila-cloudburst.png" title="Vanilla Cloudburst" button_name="Add to cart" price="79$" />
+        <Card image=".\images\harvest-blend.png" title="Harvest Blend" button_name="Add to cart" price="90$" />
+        <Card image=".\images\velvet-fusion.png" title="Velvet Fusion" button_name="Add to cart" price="98$" />
+        <Card image=".\images\peak-blend.png" title="Peak Blend" button_name="Add to cart" price="80$" />
+        <Card image=".\images\morning-symphony.png" title="Bistro Reserve" button_name="Add to cart" price="79$" />
       </div>
+    </div>
+  </div>
+</div>
+
     );
   }
   
--- src/Sections/Section_Exploring.jsx
+++ src/Sections/Section_Exploring.jsx
@@ -6,25 +6,38 @@ function Section_Exploring () {
 
 
 
-      
-      <div className="app-container bg-[#6d4c41] text-white font-sans text-center ">
-        <div className="relative bg-cover bg-center bg-[url('https://www.albayan.ae/assets/archives/images/2018/11/15/3410328.jpg')]">
-        <h1 className="text-white relative z-10">Exploring Our Unique Collections </h1>
-        <div className="absolute inset-0 bg-[#6d4c41]/80"></div> 
-  
-          <div className="min-h-screen flex items-center justify-center">
-  
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 min-h-screen text-center items-center justify-center relative">
-              <Card image=".\images\black-roast.png" title="Beautiful Sunset" />
-              <Card image=".\images\bulk-packs.png" title="Bulk Packs" />
-              <Card image=".\images\iced-coffees.png" title="Iced Coffees" />
-              <Card image=".\images\velvet-roast.png" title="Blended" />
-              <Card image=".\images\cold-brews.png" title="Cold Brews" />
-              <Card image=".\images\instant-made.png" title="Instant Made" />
-            </div>
+      <div className="app-container bg-[#6d4c41] text-white font-sans text-center">
+      {/* الخلفية بالفيديو */}
+      <div className="relative bg-cover bg-center">
+        {/* عنصر الفيديو */}
+        <video 
+          autoPlay 
+          loop 
+          muted 
+          playsInline 
+          className="absolute inset-0 w-full h-full object-cover z-0">
+          <source src="https://videos.pexels.com/video-files/4326938/4326938-uhd_3840_2160_25fps.mp4" type="video/mp4" />
+          {/* رسالة في حالة عدم دعم الفيديو */}
+          Your browser does not support the video tag.
+        </video>
+    
+        {/* النص والمحتوى فوق الفيديو */}
+        <h1 className="text-white relative z-10">Exploring Our Unique Collections</h1>
+        <div className="absolute inset-0 bg-[#6d4c41]/80 z-5"></div>
+    
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 min-h-screen text-center items-center justify-center relative z-10">
+            <Card image=".\images\black-roast.png" title="Beautiful Sunset" />
+            <Card image=".\images\bulk-packs.png" title="Bulk Packs" />
+            <Card image=".\images\iced-coffees.png" title="Iced Coffees" />
+            <Card image=".\images\velvet-roast.png" title="Blended" />
+            <Card image=".\images\cold-brews.png" title="Cold Brews" />
+            <Card image=".\images\instant-made.png" title="Instant Made" />
           </div>
         </div>
       </div>
+    </div>
+    
     );
   }
   
